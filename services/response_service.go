e85444a20869116ec1219870463a9851b3a88237
Lucky Pratama <luckypratama71@yahoo.com>
2019-03-25 20:08:32 +0700

    Refactoring

--- services/response_service.go
+++ services/response_service.go
@@ -2,10 +2,11 @@ package services
 
 import (
 	m "go-mux-bigquery-app-engine/models"
+	"net/http"
 )
 
 func ResponseSuccess(data interface{}) m.Response {
-	resp := m.Response{ResponseCode: 200, ResponseDesc: "Ok", Data: data, DebugInfo: ""}
+	resp := m.Response{ResponseCode: http.StatusOK, ResponseDesc: http.StatusText(http.StatusOK), Data: data, DebugInfo: ""}
 	return resp
 }
 
